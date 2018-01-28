b677d81ad93effa35538288bfb381705de153fff
Jim McGee <jmcgee86@gmail.com>
2018-01-28 17:03:20 +0000

    displaying basic info for one car using controller

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-var app = angular.module('carRental', [ ]);
+(function(){
+    
+    var app = angular.module('carRental', [ ]);
+    
+    app.controller('RentalController', function(){
+        this.product = car;
+        
+    });
+    
+    var car = {
+        price: 55,
+        carType: "Toyota Corolla",
+        description: "description of car",
+        totalCars: 20,
+        rented: 0
+    }
+    
+})();
