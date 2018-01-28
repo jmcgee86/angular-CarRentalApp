69635211fc9a0218be706d8d321be737c06b9ea1
Jim McGee <jmcgee86@gmail.com>
2018-01-28 18:06:06 +0000

    displays images using image directive

--- app.js
+++ app.js
@@ -12,6 +12,13 @@
             carType: "Toyota Corolla",
             price: 55,
             description: "description of car",
+            mpg:"30 City / 40 Highway",
+            images:[
+                {
+                    full: 'images/corolla-full.jpeg',
+                    thumb: 'images/corolla-thumb.jpeg'
+                }
+                ],
             totalCars: 20,
             rented: 0,
             //canRent: true,
@@ -21,6 +28,13 @@
             carType: "Toyota Prius",
             price: 75,
             description: "description of car",
+            mpg: "58 City / 53 Highway",
+            images: [
+                {
+                  full: 'images/prius-full.jpeg',
+                  thumb: 'images/prius-thumb.jpeg'
+                }
+                ],
             totalCars: 20,
             rented: 0,
             //canRent: false,
@@ -30,6 +44,13 @@
             carType: "Toyota Rav4",
             price: 95,
             description: "description of car",
+            mpg: "23 City / 30 Highway",
+            images: [
+                {
+                    full: 'images/rav4-full.jpeg',
+                    thumb: 'images/rav4-thumb.jpeg'
+                }
+                ],
             totalCars: 20,
             rented: 0,
             //canRent: true,
