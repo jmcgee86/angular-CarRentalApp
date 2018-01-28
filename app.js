ba5eb166e6b593b76e8423dc03691cecfca55021
Jim McGee <jmcgee86@gmail.com>
2018-01-28 17:43:40 +0000

    displaying multiple car's info using directives

--- app.js
+++ app.js
@@ -3,18 +3,37 @@
     var app = angular.module('carRental', [ ]);
     
     app.controller('RentalController', function(){
-        this.product = car;
+        this.products = cars;
         
     });
     
-    var car = {
-        price: 55,
-        carType: "Toyota Corolla",
-        description: "description of car",
-        totalCars: 20,
-        rented: 0,
-        canRent: true,
-        noneLeft: false
-    }
-    
+    var cars = [
+        {
+            carType: "Toyota Corolla",
+            price: 55,
+            description: "description of car",
+            totalCars: 20,
+            rented: 0,
+            //canRent: true,
+            noneLeft: false
+        },
+        {
+            carType: "Toyota Prius",
+            price: 75,
+            description: "description of car",
+            totalCars: 20,
+            rented: 0,
+            //canRent: false,
+            noneLeft: false
+        },
+        {
+            carType: "Toyota Rav4",
+            price: 95,
+            description: "description of car",
+            totalCars: 20,
+            rented: 0,
+            //canRent: true,
+            noneLeft: false
+        }
+    ];
 })();
